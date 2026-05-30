bd21bbd9cf515e0901d95e2085875454612e94b2
baxtiyorovanozima02 <baxtiyorovanozima02@gmail.com>
2026-05-30 10:49:26 +0100

    feat: add Vocabulary and Statistics routes to App

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-d
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
 import DashboardPage from './pages/DashboardPage';
+import VocabularyPage from './pages/VocabularyPage';
+import StatisticsPage from './pages/StatisticsPage';
 
 function PrivateRoute({ children }) {
     const token = localStorage.getItem('access_token');
     return token ? children : <Navigate to="/login" />;
 }
+
 
 function App() {
     return (
@@ -14,11 +17,9 @@ function App() {
             <Routes>
                 <Route path="/login" element={<LoginPage />} />
                 <Route path="/register" element={<RegisterPage />} />
-                <Route path="/dashboard" element={
-                    <PrivateRoute>
-                        <DashboardPage />
-                    </PrivateRoute>
-                } />
+                <Route path="/dashboard" element={<PrivateRoute><DashboardPage /></PrivateRoute>} />
+                <Route path="/vocabulary" element={<PrivateRoute><VocabularyPage /></PrivateRoute>} />
+                <Route path="/statistics" element={<PrivateRoute><StatisticsPage /></PrivateRoute>} />
                 <Route path="/" element={<Navigate to="/login" />} />
             </Routes>
         </Router>
